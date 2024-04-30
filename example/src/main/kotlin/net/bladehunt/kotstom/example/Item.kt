40bf22cc2dc36837715a10c58b9a4d5fb2725e79
oglass <alexbirdand@gmail.com>
2024-04-30 15:01:40 -0400

    Change item example

--- example/src/main/kotlin/net/bladehunt/kotstom/example/Item.kt
+++ example/src/main/kotlin/net/bladehunt/kotstom/example/Item.kt
@@ -6,10 +6,15 @@ import net.minestom.server.item.ItemComponent
 import net.minestom.server.item.Material
 
 val exampleItem = item(Material.WRITTEN_BOOK) {
+    // customName and itemName are now separate
     customName = "Hey".asMini()
     amount = 5
     lore {
         +"<red>hello"
     }
+    // Components can be set like so
     ItemComponent.CUSTOM_MODEL_DATA(5)
+
+    // ... or with the normal one - you decide
+    set(ItemComponent.CUSTOM_MODEL_DATA, 5)
 }
