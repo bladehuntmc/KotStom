b64c6cca5e3f9f4211a9a47a35e2a27fb65f18ac
oglass <alexbirdand@gmail.com>
2024-06-02 12:47:51 -0400

    fix: Existing tests

--- src/test/kotlin/net/bladehunt/kotstom/test/AdventureTest.kt
+++ src/test/kotlin/net/bladehunt/kotstom/test/AdventureTest.kt
@@ -2,17 +2,18 @@ package net.bladehunt.kotstom.test
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import net.bladehunt.kotstom.extension.italic
-import net.bladehunt.kotstom.extension.undecorate
+import net.bladehunt.kotstom.extension.adventure.italic
+import net.bladehunt.kotstom.extension.adventure.undecorate
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 
-class AdventureTest : FunSpec({
-    test("should unformat decorations") {
-        val original = Component.text("Component").italic()
-        original.decorations()[TextDecoration.ITALIC] shouldBe TextDecoration.State.TRUE
+class AdventureTest :
+    FunSpec({
+        test("should unformat decorations") {
+            val original = Component.text("Component").italic()
+            original.decorations()[TextDecoration.ITALIC] shouldBe TextDecoration.State.TRUE
 
-        val new = original.undecorate(TextDecoration.ITALIC)
-        new.decorations()[TextDecoration.ITALIC] shouldBe TextDecoration.State.FALSE
-    }
-})
+            val new = original.undecorate(TextDecoration.ITALIC)
+            new.decorations()[TextDecoration.ITALIC] shouldBe TextDecoration.State.FALSE
+        }
+    })
