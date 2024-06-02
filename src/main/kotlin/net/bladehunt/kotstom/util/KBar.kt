06928348c1ea554fe8b9b256f2beeb4c00dd3f9f
oglass <alexbirdand@gmail.com>
2024-06-02 12:50:18 -0400

    feat: setTitle

--- src/main/kotlin/net/bladehunt/kotstom/util/KBar.kt
+++ src/main/kotlin/net/bladehunt/kotstom/util/KBar.kt
@@ -7,8 +7,14 @@ import net.minestom.server.entity.Player
 import net.minestom.server.scoreboard.Sidebar
 import net.minestom.server.scoreboard.Sidebar.ScoreboardLine
 
+/**
+ * A Sidebar with automatic line placements
+ *
+ * @param title The original title of the Sidebar
+ * @author oglassdev
+ */
 class KBar(title: Component = Component.empty()) : Viewable {
-    val sidebar: Sidebar = Sidebar(title)
+    private val sidebar: Sidebar = Sidebar(title)
     private val lineId: AtomicInteger = AtomicInteger(-1)
 
     inner class Line(display: Component = Component.empty()) {
@@ -49,6 +55,10 @@ class KBar(title: Component = Component.empty()) : Viewable {
 
     fun addLine(line: Line) {
         lines.add(line)
+    }
+
+    fun setTitle(title: Component) {
+        sidebar.setTitle(title)
     }
 
     override fun addViewer(player: Player): Boolean = sidebar.addViewer(player)
