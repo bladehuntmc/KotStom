fc73c7c085af5d27892ced57cadcef27cb7cd374
oglass <alexbirdand@gmail.com>
2024-06-23 00:12:11 -0400

    chore: unit tests 🙄

---
+++ adventure-serialization/src/test/kotlin/net/bladehunt/kotstom/test/AllTest.kt
@@ -0,0 +1,35 @@
+package net.bladehunt.kotstom.test
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.Serializable
+import net.bladehunt.kotstom.serialization.adventure.decodeFromCompound
+import net.bladehunt.kotstom.serialization.adventure.encodeToCompound
+
+@Serializable data class Everything(val listData: ListData, val data: Data, val poly: Poly)
+
+class AllTest :
+    FunSpec({
+        test("everything should work") {
+            val valueToSerialize =
+                Everything(
+                    ListData(listOf("abc", "def", "ghi"), listOf(0, 1, 2), listOf(0.1, 1.2, 2.4)),
+                    Data(
+                        Data.Inner("John Doe", "The Earth", 40),
+                        1,
+                        2,
+                        3,
+                        4.314159F,
+                        5.314159,
+                        Data.Inner("Jane Doe", "John's House", 39),
+                        "Value",
+                        true),
+                    Poly.Wants("cracker"))
+
+            val valueAsCompound = encodeToCompound(valueToSerialize)
+
+            val decodedValue = decodeFromCompound<Everything>(valueAsCompound)
+
+            valueToSerialize shouldBe decodedValue
+        }
+    })
